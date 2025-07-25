46cf418d992de7a6680955413fbd2f5d590dea8d
Yusran <60111265+yusrankun@users.noreply.github.com>
2025-07-25 12:32:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit.components.v1 as stc
 @st.cache_resource
 def load_model():
     try:
-        model = joblib.load("best_model_RandomForest.pkl")
+        model = joblib.load("best_model_RandomForest_compatible.pkl")
         st.success("✅ Model loaded successfully!")
         return model
     except Exception as e:
